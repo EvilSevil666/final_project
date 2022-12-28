15bfab0e80bede40d88f21dded126ceba134c05b
EvilSevil666 <115174811+EvilSevil666@users.noreply.github.com>
2022-12-28 16:43:29 +0300

    add solution

---
+++ solution/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+Console.WriteLine("Программа которая из введённого массива строк формирует массив из строк, длина которых меньше либо равна 3 символа."); 
+
+string[] array = NArray();
+string[] result = FindLess(array, 3);
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] FindLess(string[] input, int n) {
+    string[] output = new string[CLess(input, n)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int CLess(string[] input, int n) {
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] NArray() {
+    Console.Write("Введите данные через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
